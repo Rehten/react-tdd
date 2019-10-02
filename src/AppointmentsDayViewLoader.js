d1daf5d6e73dbcbbf493297dbd4e77b392c66368
rehten <Sithas@yandex.ru>
2019-10-02 17:51:11 +0300

    add test spy

--- src/AppointmentsDayViewLoader.js
+++ src/AppointmentsDayViewLoader.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import {AppointmentsDayView} from "./AppointmentsDayView";
 
-export const AppointmentsDayViewLoader = ({today}) => {
+export const AppointmentsDayViewLoader = ({today, appointments}) => {
     const from = today.setHours(0, 0, 0, 0);
     const to = today.setHours(23, 59, 59, 999);
 
@@ -18,5 +19,9 @@ export const AppointmentsDayViewLoader = ({today}) => {
         fetchAppointments();
     }, [from, to]);
 
-    return null;
+    return <AppointmentsDayView appointments={[]} />;
+};
+
+AppointmentsDayViewLoader.defaultProps = {
+    today: new Date()
 };
--- test/AppointmentsDayViewLoader.test.js
+++ test/AppointmentsDayViewLoader.test.js
@@ -4,7 +4,7 @@ import {createContainer} from './domManipulators';
 import {fetchResponseOk} from './spyHelpers';
 import * as AppointmentFormExports from "../src/AppointmentForm";
 import {AppointmentFormLoader} from "../src/AppointmentFormLoader";
-import {AppointmentsDayViewLoader} from "../src/AppointmentsDayViewLoader";
+import * as AppointmentsDayViewLoaderExports from "../src/AppointmentsDayViewLoader";
 
 describe('AppointmentsDayViewLoader', () => {
     let renderAndWait, render, container;
@@ -13,15 +13,18 @@ describe('AppointmentsDayViewLoader', () => {
         {startsAt: today.setHours(9, 0, 0, 0)},
         {startsAt: today.setHours(10, 0, 0, 0)}
     ];
+    const {AppointmentsDayViewLoader} = AppointmentsDayViewLoaderExports;
 
     beforeEach(() => {
         ({renderAndWait, render, container} = createContainer());
 
         jest.spyOn(window, 'fetch').mockReturnValue(fetchResponseOk(appointments));
+        jest.spyOn(AppointmentsDayViewLoaderExports, 'AppointmentsDayViewLoader').mockReturnValue(null);
     });
 
     afterEach(() => {
         window.fetch.mockRestore();
+        AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader.mockRestore();
     });
 
     it('fetches appointments happening today when component is mounted', async () => {
@@ -39,6 +42,15 @@ describe('AppointmentsDayViewLoader', () => {
                     'Content-Type': 'application/json'
                 }
             })
+        );
+    });
+
+    it('initially passes no data to AppointmentsDayView', async () => {
+        await renderAndWait(<AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader />);
+
+        expect(AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader).toHaveBeenCalledWith(
+            {},
+            expect.anything()
         );
     });
 });
