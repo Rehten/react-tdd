b3f2610ae5ea5c1e4ee190813a7727baf7dd2892
rehten <Sithas@yandex.ru>
2019-10-02 18:17:18 +0300

    add test spy

--- src/AppointmentsDayViewLoader.js
+++ src/AppointmentsDayViewLoader.js
@@ -1,25 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {AppointmentsDayView} from "./AppointmentsDayView";
 
-export const AppointmentsDayViewLoader = ({today, appointments}) => {
+export const AppointmentsDayViewLoader = ({today}) => {
     const from = today.setHours(0, 0, 0, 0);
     const to = today.setHours(23, 59, 59, 999);
+    const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const fetchAppointments = () => {
-            window.fetch(`/appointments/${from}-${to}`, {
+        const fetchAppointments = async () => {
+            const rslt = await window.fetch(`/appointments/${from}-${to}`, {
                 method: 'GET',
                 credentials: 'same-origin',
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
+
+            setAppointments(await rslt.json());
         };
 
         fetchAppointments();
     }, [from, to]);
 
-    return <AppointmentsDayView appointments={[]} />;
+    return <AppointmentsDayView appointments={appointments} />;
 };
 
 AppointmentsDayViewLoader.defaultProps = {
--- test/AppointmentsDayViewLoader.test.js
+++ test/AppointmentsDayViewLoader.test.js
@@ -5,13 +5,34 @@ import {fetchResponseOk} from './spyHelpers';
 import * as AppointmentFormExports from "../src/AppointmentForm";
 import {AppointmentFormLoader} from "../src/AppointmentFormLoader";
 import * as AppointmentsDayViewLoaderExports from "../src/AppointmentsDayViewLoader";
+import * as AppointmentsDayViewExports from "../src/AppointmentsDayView";
 
 describe('AppointmentsDayViewLoader', () => {
     let renderAndWait, render, container;
     const today = new Date();
     const appointments = [
-        {startsAt: today.setHours(9, 0, 0, 0)},
-        {startsAt: today.setHours(10, 0, 0, 0)}
+        {
+            startsAt: today.setHours(9, 0, 0, 0),
+            customer: {
+                firstName: 'Alex',
+                lastName: 'Alex'.split('').reverse().join(''),
+                phoneNumber: `+ 7 (${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}) ${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}-${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}-${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}`,
+                stylist: 'Jordan',
+                service: 'Taxi',
+                notes: '18-year old girl'
+            }
+        },
+        {
+            startsAt: today.setHours(10, 0, 0, 0),
+            customer: {
+                firstName: 'Alex',
+                lastName: 'Alex'.split('').reverse().join(''),
+                phoneNumber: `+ 7 (${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}) ${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}-${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}-${Math.round(Math.random() * 10)}${Math.round(Math.random() * 10)}`,
+                stylist: 'Jordan',
+                service: 'Taxi',
+                notes: '18-year old girl'
+            }
+        }
     ];
     const {AppointmentsDayViewLoader} = AppointmentsDayViewLoaderExports;
 
@@ -20,6 +41,7 @@ describe('AppointmentsDayViewLoader', () => {
 
         jest.spyOn(window, 'fetch').mockReturnValue(fetchResponseOk(appointments));
         jest.spyOn(AppointmentsDayViewLoaderExports, 'AppointmentsDayViewLoader').mockReturnValue(null);
+        jest.spyOn(AppointmentsDayViewExports, 'AppointmentsDayView').mockReturnValue(null);
     });
 
     afterEach(() => {
@@ -31,7 +53,7 @@ describe('AppointmentsDayViewLoader', () => {
         const from = today.setHours(0, 0, 0, 0);
         const to = today.setHours(23, 59, 59, 999);
 
-        await renderAndWait(<AppointmentsDayViewLoader today={today} />);
+        await renderAndWait(<AppointmentsDayViewLoader today={today}/>);
 
         expect(window.fetch).toHaveBeenCalledWith(
             `/appointments/${from}-${to}`,
@@ -46,11 +68,24 @@ describe('AppointmentsDayViewLoader', () => {
     });
 
     it('initially passes no data to AppointmentsDayView', async () => {
-        await renderAndWait(<AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader />);
+        await renderAndWait(<AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader/>);
 
-        expect(AppointmentsDayViewLoaderExports.AppointmentsDayViewLoader).toHaveBeenCalledWith(
-            {},
+        expect(AppointmentsDayViewExports.AppointmentsDayView).toHaveBeenCalledWith(
+            {
+                appointments: []
+            },
             expect.anything()
         );
+    });
+
+    it('displays time slots that are fetched on mount', async () => {
+        await renderAndWait(<AppointmentsDayViewLoader/>);
+
+        expect(AppointmentsDayViewExports.AppointmentsDayView).toHaveBeenLastCalledWith(
+            {
+                appointments
+            },
+            expect.anything()
+        )
     });
 });
