6822317f0782e432b0c5c44a573da36b70611c68
rehten <Sithas@yandex.ru>
2019-10-02 18:51:20 +0300

    add test spy

--- src/AppointmentFormLoader.js
+++ src/AppointmentFormLoader.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {AppointmentForm} from "./AppointmentForm";
 
-export const AppointmentFormLoader = () => {
+export const AppointmentFormLoader = (props) => {
     const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
 
     useEffect(() => {
@@ -20,5 +20,5 @@ export const AppointmentFormLoader = () => {
         fetchAvailableTimeSlots();
     }, []);
 
-    return (<AppointmentForm availableTimeSlots={availableTimeSlots} />);
+    return (<AppointmentForm {...props} availableTimeSlots={availableTimeSlots} />);
 };
